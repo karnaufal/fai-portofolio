7efee2d9b0f57c737d54939fdefedd9ea5dc14be
Naufal S <naufallsf@gmail.com>
2026-04-08 16:00:32 +0700

    chore: silence hydration warnings from browser extensions for good

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning={true}>
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        suppressHydrationWarning={true}
+      >
         {children}
       </body>
     </html>
