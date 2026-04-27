e4b5a61c4a5611e07f69128b0d18e57052695b30
Naufal S <naufallsf@gmail.com>
2026-04-27 20:40:54 +0700

    feat(layout): implement rolex-style reveal footer animation

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Footer from "@/components/footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -11,7 +12,6 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
-
 
 export const metadata: Metadata = {
   title: "Fai Portofolio",
@@ -30,10 +30,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning={true}>
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         suppressHydrationWarning={true}
       >
-        {children}
+        <Footer />
+
+        <div className="relative z-10 mb-[40vh] md:mb-[50vh]">
+          {children}
+        </div>
+
       </body>
     </html>
   );
